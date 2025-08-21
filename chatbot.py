d0add78e45d9a02628a065f4630e186e7a402ca3
Young Nika <85286129+minhnq1402@users.noreply.github.com>
2025-08-22 03:27:51 +0700

    fix ##

--- chatbot.py
+++ chatbot.py
@@ -2,45 +2,34 @@ import streamlit as st
 from hugchat import hugchat
 from hugchat.login import Login
 
-# App title
-st.title('Simple ChatBot')
+st.title('Simple Chat Bot')
 
-# Hugging Face Credentials
+
 with st.sidebar:
-    st.title('Login HugChat')
-    hf_email = st.text_input('Enter E-mail:')
-    hf_pass = st.text_input('Enter Password:', type='password')
-    if not (hf_email and hf_pass):
-        st.warning('Please enter your account!', icon='⚠️')
-    else:
-        st.success('Proceed to entering your prompt message!', icon='👉')
-   
-
-# Store LLM generated responses
+    st.title('Huggingface Account')
+    hf_email = st.text_input('E-mail:')
+    hf_pass = st.text_input('Password:', type='password')
+    
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "Tôi có thể giúp gì cho bạn?"}]
 
-# Display chat messages
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# Function for generating LLM response
 def generate_response(prompt_input, email, passwd):
-    # Hugging Face Login
     sign = Login(email, passwd)
-    cookies = sign.login()
-    # Create ChatBot                        
+    cookies = sign.login()                      
     chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
     return chatbot.chat(prompt_input)
 
-# User-provided prompt
 if prompt := st.chat_input(disabled=not (hf_email and hf_pass)):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
 
-# Generate a new response if last message is not from assistant
+
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
--- object_detection.py
+++ object_detection.py
@@ -18,32 +18,25 @@ def process_image(image):
 def annotate_image(
         image, detections, confidence_threshold=0.5
     ):
-    # loop over the detections
     (h, w) = image.shape[:2]
     for i in np.arange(0, detections.shape[2]):
         confidence = detections[0, 0, i, 2]
 
         if confidence > confidence_threshold:
-            # extract the index of the class label from the `detections`,
-            # then compute the (x, y)-coordinates of the bounding box for
-            # the object
             idx = int(detections[0, 0, i, 1])
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
             cv2.rectangle(image, (startX, startY), (endX, endY), 70, 2)
     return image
 
-def main():
-    st.title('Object Detection for Images')
-    file = st.file_uploader('Upload Image', type = ['jpg','png','jpeg'])
-    if file is not None:
-        st.image(file, caption = "Uploaded Image")
 
-        image = Image.open(file)
-        image = np.array(image)
-        detections = process_image(image)
-        processed_image = annotate_image(image, detections)
-        st.image(processed_image, caption = "Processed Image")
+st.title('Object Detection for Images')
+file = st.file_uploader('Upload Image', type = ['jpg','png','jpeg'])
+if file is not None:
+    st.image(file, caption = "Uploaded Image")
 
-if __name__ == "__main__":
-    main()
+    image = Image.open(file)
+    image = np.array(image)
+    detections = process_image(image)
+    processed_image = annotate_image(image, detections)
+    st.image(processed_image, caption = "Processed Image")
